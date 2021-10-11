a22b3b3295a26347991ce4684a37a1b1ee5029f4
ddorn <diego.dorn@free.fr>
2021-10-11 15:43:29 +0200

    CozyFractal entry
    commit 5f32ce6db77af4bbb4e5fbf51601adfa3b800a67
    Author: ddorn <diego.dorn@free.fr>
    Date:   Mon Oct 11 15:39:15 2021 +0200
    
        reformat
    
    commit 4629181afd8bb5dada8161268f98f52906554ea7
    Author: ddorn <diego.dorn@free.fr>
    Date:   Thu Oct 7 10:10:27 2021 +0200
    
        wclib: better exception handling
    
    commit 45bb395d2fdb6f7ae2f4d86faac6b393a92791ad
    Author: ddorn <diego.dorn@free.fr>
    Date:   Wed Oct 6 20:24:37 2021 +0200
    
        use numpy faster memory simulation
    
    commit 48ff1cca3c59a319f2244de06a6a17b8015f17c7
    Author: ddorn <diego.dorn@free.fr>
    Date:   Wed Oct 6 15:36:06 2021 +0200
    
        darker memory
    
    commit c3bbcc687f94796dce6e321909cbe793c74ac333
    Author: ddorn <diego.dorn@free.fr>
    Date:   Wed Oct 6 15:35:16 2021 +0200
    
        Prompt to install modules
    
    commit 5103edd07bac41d75e826ae0ea6d4d05c221ad0b
    Author: ddorn <diego.dorn@free.fr>
    Date:   Sun Oct 3 22:59:33 2021 +0200
    
        working !

---
+++ 01-fog-of-war/cozyfractal/main.py
@@ -0,0 +1,68 @@
+import sys
+from pathlib import Path
+
+try:
+    import wclib
+except ImportError:
+    # wclib may not be in the path because of the architecture
+    # of all the challenges and the fact that there are many
+    # way to run them (through the showcase, or on their own)
+
+    ROOT_FOLDER = Path(__file__).parent.parent.parent
+    sys.path.append(str(ROOT_FOLDER))
+    import wclib  # It should work now
+
+# This line tells python how to handle the relative imports
+# when you run this file directly.
+__package__ = "01-fog-of-war." + Path(__file__).parent.name
+
+# ---- Recommended: don't modify anything above this line ---- #
+
+# Metadata about your submission
+__author__ = "CozyFractal#6978"
+__achievements__ = [  # Uncomment the ones you've done
+    # "Casual",
+    # "Ambitious",
+    # "Adventurous",
+]
+
+
+from operator import attrgetter
+import pygame
+
+from wclib import SIZE
+
+# To import the modules in yourname/, you need to use relative imports,
+# otherwise your project will not be compatible with the showcase.
+from .objects import Ghost, Player, SolidObject, Shadow
+
+BACKGROUND = 0x66856C
+
+
+def mainloop():
+    player = Player((100, 100))
+    trees = SolidObject.generate_many(36)
+    ghosts = [Ghost() for _ in range(16)]
+    shadow = Shadow(SIZE)
+
+    all_objects = trees + [player] + ghosts
+
+    clock = pygame.time.Clock()
+    while True:
+        screen, events = yield
+        for event in events:
+            if event.type == pygame.QUIT:
+                return
+
+        for obj in all_objects:
+            obj.logic(objects=all_objects)
+
+        shadow.logic(player, all_objects)
+
+        screen.fill(BACKGROUND)
+
+        shadow.draw(screen, player, all_objects)
+
+
+if __name__ == "__main__":
+    wclib.run(mainloop())
