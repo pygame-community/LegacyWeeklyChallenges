d9ee2470ddf3f366953bfcf11e73146709cc806f
ddorn <diego.dorn@free.fr>
2021-10-11 14:38:25 +0200

    RoboMarchello entry

---
+++ 01-fog-of-war/RoboMarchello/objects.py
@@ -0,0 +1,181 @@
+"""
+This file provides objects that can be used to make up
+a basic playground for the challenges.
+
+This code is provided so that you can focus on implementing
+a fog of war, without needed
+Feel free to modify everything in this file to your liking.
+"""
+from random import choice, gauss
+import math
+import pygame,pygame.gfxdraw
+
+from wclib import SIZE
+from .utils import clamp, from_polar, load_image, random_in_rect
+
+SCREEN = pygame.Rect(0, 0, *SIZE)
+
+class Object:
+    """The base class for all objects of the game."""
+
+    def __init__(self, pos, sprite: pygame.Surface):
+        self.pos = pygame.Vector2(pos)
+        self.size = pygame.Vector2(sprite.get_size())
+        self.sprite = sprite
+
+    def __str__(self):
+        return f"<{self.__class__.__name__}(pos={self.pos}, size={self.size})>"
+
+    @property
+    def rect(self):
+        return pygame.Rect(self.pos, self.size)
+
+    def draw(self, screen):
+        screen.blit(self.sprite, self.pos)
+
+    def logic(self, **kwargs):
+        pass
+
+
+class Object8Directional(Object):
+    SCALE = 3
+    SIZE = 16
+    SHEET = "blue_ghost"
+
+    ACCELERATION = 0.3
+    DAMPING = 0.85
+
+    # Asymptotic velocity will be solution of
+    # x = x * DAMPING + ACCELERATION
+    # That is,
+    # x = ACCELERATION / (1 - DAMPING)
+
+    def __init__(self, pos):
+        self.velocity = pygame.Vector2()
+        self.acceleration = pygame.Vector2()
+
+        super().__init__(pos, self.get_image())
+
+    def get_image(self):
+        angle = self.velocity.as_polar()[1]
+        idx = int((-angle + 90 + 45 / 2) % 360 / 360 * 8)
+        sheet = load_image(self.SHEET, self.SCALE)
+        unit = self.SIZE * self.SCALE
+        return sheet.subsurface(idx * unit, 0, unit, unit)
+
+    def logic(self, **kwargs):
+        self.velocity *= self.DAMPING
+        self.velocity += self.acceleration
+        self.pos += self.velocity
+        self.sprite = self.get_image()
+
+        self.pos.x = clamp(self.pos.x, 0, SIZE[0])
+        self.pos.y = clamp(self.pos.y, 0, SIZE[1])
+
+
+class Player(Object8Directional):
+    def logic(self, **kwargs):
+        pressed = pygame.key.get_pressed()
+        direction_x = pressed[pygame.K_RIGHT] - pressed[pygame.K_LEFT]
+        direction_y = pressed[pygame.K_DOWN] - pressed[pygame.K_UP]
+
+        self.acceleration = pygame.Vector2(direction_x, direction_y) * self.ACCELERATION
+        super().logic(**kwargs)
+
+
+class Ghost(Object8Directional):
+    SHEET = "pink_ghost"
+    ACCELERATION = 0.2
+    DAMPING = 0.9
+
+    def __init__(self, pos=None):
+        if pos is None:
+            pos = random_in_rect(SCREEN)
+        super().__init__(pos)
+        self.goal = self.new_goal()
+
+    def new_goal(self):
+        direction = from_polar(60, gauss(self.velocity.as_polar()[1], 30))
+        return self.rect.center + direction
+
+    def logic(self, **kwargs):
+        middle_area = SCREEN.inflate(-30, -30)
+        while self.rect.collidepoint(self.goal) or not middle_area.collidepoint(
+            self.goal
+        ):
+            self.goal = self.new_goal()
+
+        self.acceleration = (
+            self.goal - self.rect.center
+        ).normalize() * self.ACCELERATION
+        super().logic(**kwargs)
+
+
+class SolidObject(Object):
+    SHEET_RECT = [
+        (0, 0, 24, 31),
+        (24, 0, 24, 24),
+        (48, 0, 24, 24),
+        (72, 0, 16, 24),
+        (88, 0, 48, 43),
+        (136, 0, 16, 16),
+    ]
+    SCALE = 3
+
+    def __init__(self, pos):
+        sheet = load_image("tileset", self.SCALE)
+        sheet.set_colorkey(0xFFFFFF)
+        rect = choice(self.SHEET_RECT)
+        rect = [x * self.SCALE for x in rect]
+        super().__init__(pos, sheet.subsurface(rect))
+
+    @classmethod
+    def generate_many(cls, nb=16, max_tries=1000):
+        objects = []
+        tries = 0  # avoids infinite loop
+        while len(objects) < nb and tries < max_tries:
+            pos = random_in_rect(SCREEN)
+            obj = cls(pos)
+            if not any(obj.rect.colliderect(other.rect) for other in objects):
+                objects.append(obj)
+        return objects
+
+class Fog():
+    def __init__(self,player):
+        self.maskSurf = pygame.Surface(SCREEN.size,pygame.SRCALPHA)
+        self.maskSurf.fill((0,0,0))
+
+        self.player = player
+
+        self.addRadius = 0
+        self.isAdd = False
+
+        self.rect = pygame.Rect(0,0,125*2,125*2)
+
+    def draw(self,screen):
+        if self.addRadius <= 0:
+            self.isAdd = True
+        elif self.addRadius >= 10:
+            self.isAdd = False
+
+        if self.isAdd == True:
+            self.addRadius += 0.2
+        else:
+            self.addRadius -= 0.2
+            
+        smoothRadius = 135#225
+        
+        alpha = 210
+        
+        for alp in range(200):
+            alpha -= 1
+            pygame.draw.circle(self.maskSurf,(0,0,0,alpha),self.player.rect.center,smoothRadius)
+            smoothRadius -= 1
+        self.rect.center = self.player.rect.center
+
+        screen.blit(self.maskSurf,(0,0))
+
+    def get_dist(self,ghost):
+        dist = [(self.player.rect.center[0]-ghost.rect.center[0])**2,(self.player.rect.center[1]-ghost.rect.center[1])**2]
+
+        return math.sqrt(dist[0]+dist[1])
