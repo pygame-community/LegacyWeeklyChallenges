51b23983a1490dc7d5fd328afd41d42e3277da97
tank king <72032294+tank-king@users.noreply.github.com>
2021-11-14 17:50:53 +0530

    Create metadata.py

---
+++ 01-fog-of-war/Blaatand29/metadata.py
@@ -0,0 +1,10 @@
+class Metadata:
+    discord_tag = "Blaatand29#0070"
+    display_name = "Blaatand29"
+    achievements = [
+        "Casual",
+        "Ambitious",
+        "Adventurous",
+    ]
+    min_python_version = (3, 6)
+    dependencies = []
