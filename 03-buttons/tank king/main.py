d0c9ae6e167ba4f64aacbbc2a67965a922c40409
tank king <72032294+tank-king@users.noreply.github.com>
2021-11-14 09:51:40 +0530

    Update main.py

--- 03-buttons/tank king/main.py
+++ 03-buttons/tank king/main.py
@@ -21,14 +21,6 @@ __package__ = "03-buttons." + Path(__file__).absolute().parent.name
 
 # ---- Recommended: don't modify anything above this line ---- #
 
-# Metadata about your submission
-__author__ = "CozyFractal#0042"  # Put yours!
-__achievements__ = [  # Uncomment the ones you've done
-    # "Casual",
-    # "Ambitious",
-    # "Adventurous",
-]
-
 import pygame
 
 if not pygame.get_init():
