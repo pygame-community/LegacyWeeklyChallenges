79d0808dd88e4b69fac055beefa9a5fd5e328b65
tank king <72032294+tank-king@users.noreply.github.com>
2021-11-14 09:11:22 +0530

    Create metadata.py

---
+++ 03-buttons/RoboMarchello/metadata.py
@@ -0,0 +1,10 @@
+class Metadata:
+    discord_tag = "RoboMarchello#0570"
+    display_name = "RoboMarchello"
+    achievements = [
+        "Casual",
+        "Ambitious",
+        "Adventurous",
+    ]
+    min_python_version = (3, 6)
+    dependencies = []
