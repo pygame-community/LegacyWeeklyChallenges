17e91c44dcdcb2c19f5ec8b1a9b7862537e27227
tank king <72032294+tank-king@users.noreply.github.com>
2021-11-14 21:06:37 +0530

    Create metadata.py

---
+++ 01-fog-of-war/MegaIng/metadata.py
@@ -0,0 +1,10 @@
+class Metadata:
+    discord_tag = "MegaIng4572#7777"
+    display_name = "MegaIng"
+    achievements = [
+        "Casual",
+        "Ambitious",
+        "Adventurous",
+    ]
+    min_python_version = (3, 6)
+    dependencies = []
