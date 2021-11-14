e4cd1a71e2cbc4a2df772d82a6ba00b52ca159e9
tank king <72032294+tank-king@users.noreply.github.com>
2021-11-14 21:32:05 +0530

    Update main.py

--- 01-fog-of-war/grimmys/main.py
+++ 01-fog-of-war/grimmys/main.py
@@ -20,14 +20,6 @@ __package__ = "01-fog-of-war." + Path(__file__).parent.name
 
 # ---- Recommended: don't modify anything above this line ---- #
 
-# Metadata about your submission
-__author__ = "Grimmys#0373"
-__achievements__ = [  # Uncomment the ones you've done
-    "Casual",
-    "Ambitious",
-    "Adventurous",
-]
-
 from operator import attrgetter
 import pygame
 
