e45d68d9918aced0f98d6c3d51efa1dc0499ec48
tank king <72032294+tank-king@users.noreply.github.com>
2021-11-14 09:07:17 +0530

    Create metadata.py

---
+++ 03-buttons/Emc235#2196/metadata.py
@@ -0,0 +1,9 @@
+class Metadata:
+    discord_tag = "Emc235#2196"
+    display_name = "Emc235#2196"
+    achievements = [
+        "Casual",
+        "Adventurous",
+    ]
+    min_python_version = (3, 6)
+    dependencies = []
