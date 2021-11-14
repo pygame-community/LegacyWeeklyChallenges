88cfb801e15b93128654aa2a66c9cafe9980a310
tank king <72032294+tank-king@users.noreply.github.com>
2021-11-14 09:25:13 +0530

    Update main.py

--- 03-buttons/fkS124/main.py
+++ 03-buttons/fkS124/main.py
@@ -18,14 +18,6 @@ __package__ = "03-buttons." + Path(__file__).absolute().parent.name
 
 # ---- Recommended: don't modify anything above this line ---- #
 
-# Metadata about your submission
-__author__ = "fkS#2984"  # Put yours!
-__achievements__ = [  # Uncomment the ones you've done
-    "Casual",
-    "Ambitious",
-    # "Adventurous",
-]
-
 import pygame
 from .app import App
 
