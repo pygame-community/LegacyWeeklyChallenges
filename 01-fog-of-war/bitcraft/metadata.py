169c5bf33154024e9046f10cf11f8d3762942d32
tank king <72032294+tank-king@users.noreply.github.com>
2021-11-14 21:25:05 +0530

    Create metadata.py

---
+++ 01-fog-of-war/bitcraft/metadata.py
@@ -0,0 +1,10 @@
+class Metadata:
+    discord_tag = "thecornboss#2284"
+    display_name = "bitcraft"
+    achievements = [
+        "Casual",
+        "Ambitious",
+        "Adventurous",
+    ]
+    min_python_version = (3, 6)
+    dependencies = []
