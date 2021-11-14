37c5c5481b88550808d1ece466eaf21d4a0941bd
tank king <72032294+tank-king@users.noreply.github.com>
2021-11-14 21:00:29 +0530

    Create metadata.py

---
+++ 01-fog-of-war/CoopERR/metadata.py
@@ -0,0 +1,10 @@
+class Metadata:
+    discord_tag = "CoopERR#2213"
+    display_name = "CoopERR"
+    achievements = [
+        "Casual",
+        "Ambitious",
+        "Adventurous",
+    ]
+    min_python_version = (3, 6)
+    dependencies = []
