4d278247d0b9c796915d559f8e15629cde955c3a
tank king <72032294+tank-king@users.noreply.github.com>
2021-11-14 21:16:06 +0530

    Create metadata.py

---
+++ 01-fog-of-war/alley-indie/metadata.py
@@ -0,0 +1,9 @@
+class Metadata:
+    discord_tag = "afaranha#4797"
+    display_name = "alley-indie"
+    achievements = [
+        "Casual",
+        "Ambitious",
+    ]
+    min_python_version = (3, 6)
+    dependencies = []
