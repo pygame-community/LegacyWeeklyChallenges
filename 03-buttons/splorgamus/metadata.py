6e013217d4f406d4bfe8ceef3f743eb4062309b8
tank king <72032294+tank-king@users.noreply.github.com>
2021-11-14 09:42:17 +0530

    Create metadata.py

---
+++ 03-buttons/splorgamus/metadata.py
@@ -0,0 +1,9 @@
+class Metadata:
+    discord_tag = "splorgamus#0257"
+    display_name = "splorgamus"
+    achievements = [
+        "Casual",
+        "Ambitious",
+    ]
+    min_python_version = (3, 6)
+    dependencies = []
